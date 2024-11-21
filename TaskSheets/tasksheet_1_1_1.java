8c243ddce55fb291177616077bc7299f7db93e6e
Jhon Mark Dizon <135515334+Makiiidev@users.noreply.github.com>
2024-11-21 06:22:55 +0000

    #

--- TaskSheets/tasksheet_1_1_1.java
+++ TaskSheets/tasksheet_1_1_1.java
@@ -1,8 +1,8 @@
 package TaskSheets;
 
-public class TaskSheets_1_1_1{
+public class tasksheet_1_1_1{
     public static void main(String[] args) {
-        System.out.println("ASdasdsd");
+        System.out.println("Hello World!");
     
     }
 }
