35542988b7e610e4b51d963cc4f7a6b552f4258b
Jhon Mark Dizon <135515334+Makiiidev@users.noreply.github.com>
2024-11-23 10:06:12 +0000

    String Methods Done

--- Programming_Challenge/String_Methods.java
+++ Programming_Challenge/String_Methods.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Sting_Methods {
+public class String_Methods {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
