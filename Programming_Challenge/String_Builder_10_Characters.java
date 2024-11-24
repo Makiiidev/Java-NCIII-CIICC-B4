40f461f6469b3144df6479a69b24d7831c75c652
Jhon Mark Dizon <135515334+Makiiidev@users.noreply.github.com>
2024-11-24 03:30:37 +0000

    String Builder Done

--- Programming_Challenge/String_Builder_10_Characters.java
+++ Programming_Challenge/String_Builder_10_Characters.java
@@ -1,6 +1,61 @@
+import java.util.Scanner;
+import java.lang.*; 
+
 public class String_Builder_10_Characters {
 
     public static void main(String[] args) {
-        System.out.println("sdasdsadsad");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("String Builder - 10 Characters");
+        System.out.println();
+        System.out.println("Enter a String of at least 10 characters");
+
+        String input;
+        input = sc.nextLine();
+        StringBuilder sb = new StringBuilder(input);
+
+
+        if (input.length() < 10) {
+            System.out.println("Error: The string must have at least 10 characters.");
+            return;
+        }
+        
+        System.out.println();
+        System.out.println("Original String: " + sb);
+
+        int length = input.length();
+        System.out.println("Length: " + sb.length());
+
+        char firstChar = sb.charAt(0);
+        System.out.println("First Character: " + firstChar);
+
+        char lastChar = sb.charAt(sb.length() - 1);
+        System.out.println("Last Character: " + lastChar);
+
+        int firstOccur = sb.indexOf("a");
+        System.out.println("Index of first occurrence of 'a': " + firstOccur);
+
+        String substringFrom = sb.substring(3, 6);
+        System.out.println("Substring from index 3 to 6: " + substringFrom);
+
+        sb.append("123");
+        System.out.println("After Append: " + sb);
+
+        sb.insert(4, "xyz");
+        System.out.println("After Insert xyz: " + sb);
+
+        sb.delete(2, 4);
+        System.out.println("After Delete index of 2 from 4: " + sb);
+
+        sb.deleteCharAt(8);
+        System.out.println("After Delete index of 8: " + sb);
+
+        sb.reverse();
+        System.out.println("After Reverse: " + sb);
+
+    
+
+
     }
 }
