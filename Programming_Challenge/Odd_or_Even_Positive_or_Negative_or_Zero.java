0ce50cf9e3a678d889ad7043e01aac9d7cfe711e
Jhon Mark Dizon <135515334+Makiiidev@users.noreply.github.com>
2024-12-07 07:52:46 +0000

    Odd_or_Even_Positive_or_Negative_or_Zero Done

---
+++ Programming_Challenge/Odd_or_Even_Positive_or_Negative_or_Zero.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Odd_or_Even_Positive_or_Negative_or_Zero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+        
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        
+    }
+}
