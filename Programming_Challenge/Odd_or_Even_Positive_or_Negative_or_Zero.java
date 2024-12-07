69906d25fa233ff90992f23ebaf28759c30452b6
Jhon Mark Dizon <135515334+Makiiidev@users.noreply.github.com>
2024-12-07 08:03:39 +0000

    MultiplicationTable Done

--- Programming_Challenge/Odd_or_Even_Positive_or_Negative_or_Zero.java
+++ Programming_Challenge/Odd_or_Even_Positive_or_Negative_or_Zero.java
@@ -4,7 +4,7 @@ public class Odd_or_Even_Positive_or_Negative_or_Zero {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number : ");
         int number = sc.nextInt();
         
         if (number % 2 == 0) {
